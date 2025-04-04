dfa5363105a5452b7a6befb17d99f51ee97d7670
taehwan01 <monte6198@gmail.com>
2025-04-04 21:30:26 +0900

    Initialize didito-spring

---
+++ src/main/java/didito/diditospring/DiditoSpringApplication.java
@@ -0,0 +1,13 @@
+package didito.diditospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiditoSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiditoSpringApplication.class, args);
+    }
+
+}
